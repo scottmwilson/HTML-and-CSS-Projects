faba55c9afa33151d2198ff23f7949496a1fed29
scottmwilson <scott@backstageconsult.com>
2023-02-04 13:34:48 -0500

    Basic HTML and JavaScript 1 Submission Assignment

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<script>
+alert("Hello, World!")
+Function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		document.getElementById("Irish").innerHTML = 
+		result;
+	}
+</script>
+</html>
