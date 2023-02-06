36d9ef76c358ae9efe6762c303ca893f238972ab
scottmwilson <scott@backstageconsult.com>
2023-02-06 17:02:30 -0500

    Send New Assignments

---
+++ Basic_HTML_and_CSS/VS Code Assignment/Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello, World!'; console.log(greeting);
+console.log(greeting);
+
+var greeting2 = 'Welcome to Node.js';
+console.log(greeting2);
+
+var sentence1 = 'Learning how to debug with the debugger';
+console.log(sentenceOne);
